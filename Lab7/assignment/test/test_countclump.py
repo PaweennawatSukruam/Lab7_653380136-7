be2cc9ed95be3cfb227de09c9e7b4cfe4a9d60f8
Paweennawat Sukruam <Paweennawat.s@kkumail.com>
2024-07-31 11:02:51 +0000

    update testcase

--- Lab7/assignment/test/test_countclump.py
+++ Lab7/assignment/test/test_countclump.py
@@ -8,16 +8,48 @@ import sys
 sys.path.insert(0,'/workspaces/Lab7_653380136-7/Lab7/assignment/clumpCount/source')
 from CountClump import CountClump
 
-class test_CountClump(unittest.TestCase):
+class test_CountClump_C_DC(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        print("Start testing class CountClump.")
+
+    @classmethod
+    def tearDownClass(cls):
+        print('\nFinish testing class CountClump.')
+
     def setUp(self):
-        self.count = count.CountClump()
-        self.nums = []
+        self.count = CountClump()
         testname = self.shortDescription()
+        if testname == "nums is None":
+            self.nums = []
+            print("\n"+testname+"\n", self.nums)
+
+        if testname == "len(nums) is 0":
+            self.nums = ""
+            print("\n"+testname+"\n", self.nums)
+
+        if testname == "All condition in loop":
+            self.nums = [1,1,2,2,2,3,4]
+            print("\n"+testname+"\n", self.nums)
     
     def tearDown(self):
         print('\nend of test', self.shortDescription())
         
-    def test_add_two_number(self):
-        """Add two numbers"""
+    def test_None(self):
+        """nums is None"""
         result = self.count.count_clumps(self.nums)
         self.assertEqual(result, 0)
+
+    def test_len_0(self):
+        """len(nums) is 0"""
+        result = self.count.count_clumps(self.nums)
+        self.assertEqual(result, 0)
+
+    def test_complete_loop(self):
+        """All condition in loop"""
+        result = self.count.count_clumps(self.nums)
+        self.assertEqual(result, 2)
+
+if __name__ == '__main__':
+    unittest.main()
